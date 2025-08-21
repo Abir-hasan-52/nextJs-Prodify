14874e1ad183c9ee85f0fff2bdb1b920ff9effa7
Abir-Hasan-52 <abirhasan5208+github@gmail.com>
2025-08-21 17:20:19 +0600

    succesfully loing user

--- src/app/login/components/LoginForm.jsx
+++ src/app/login/components/LoginForm.jsx
@@ -1,10 +1,38 @@
+"use client";
 import Link from "next/link";
+import { signIn } from "next-auth/react";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const LoginForm = () => {
-    const handleSubmit= ()=>{
-
+    const router = useRouter();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    toast("Submitting ....");
+    try {
+      const response = await signIn("credentials", {
+        email,
+        password,
+        callbackUrl: "/",
+        redirect: false,
+      });
+      if (response.ok) {
+        toast.success("Logged In successfully");
+        router.push("/");
+        form.reset();
+      } else {
+        toast.error("FAILED to Log In");
+      }
+      //console.log({ email, password });
+    } catch (error) {
+      console.log(error);
+      toast.error("FAILED to Log In");
     }
+  };
   return (
     <form onSubmit={handleSubmit} className="w-full max-w-lg space-y-8">
       <label className="form-control w-full">
